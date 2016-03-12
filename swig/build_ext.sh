22f0c1bd96076c59f41000abbc5e65b3e12fcfde
Drew Pihera <drew.pihera@gtri.gatech.edu>
2016-03-11 19:48:23 -0500

    updates to check for swig and to build the extension without running swig first (deprecating swigify.sh

--- swig/build_ext.sh
+++ swig/build_ext.sh
@@ -1,2 +1,18 @@
 #!/usr/bin/env bash
-python ./setup.py build_ext --inplace clean
+
+# path to swig executable. change this to match yours
+SWIG_EXE=/usr/bin/swig3.0
+
+# see if SWIG_EXE exists. if not, prompt the user
+${SWIG_EXE} -version >> /dev/null
+if [ $? -eq 0 ]; then
+    echo "Seems swig is available, trying to build the extension..."
+else
+    echo "The swig executable specified in this script is not available."
+    echo "If swig is installed, change the executable path in this script"
+    exit 1
+fi
+
+echo "Building extension..."
+echo "Running: python ./setup.py build_ext --swig ${SWIG_EXE}"
+python ./setup.py build_ext --swig ${SWIG_EXE} --inplace
