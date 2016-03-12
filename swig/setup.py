c699456e1d2ba3f9788fa51b0aa59482b88d74bf
Drew Pihera <drew.pihera@gtri.gatech.edu>
2016-03-11 19:54:08 -0500

    updated to use swig in the extension instead of as a separate shell script

--- swig/setup.py
+++ swig/setup.py
@@ -5,17 +5,27 @@ setup.py file for SWIG header_wrapper example
 """
 
 from distutils.core import setup, Extension
+import os.path
 
+srcs = ['header_wrapper.i',]
+inc_dirs = [os.path.abspath('../include'),]
+inc_flags = list(map(lambda x: '-I%s' % x, inc_dirs))
+
+# just need the include pathsat the moment...
+swig_opts = []
+
+swig_opts.extend(inc_flags)
 
 header_wrapper_module = Extension('_header_wrapper',
-                           sources=['_header_wrapper.c',],
-                           include_dirs = ['../include'],
-                           )
+                           sources=srcs,
+                           swig_opts=swig_opts,
+                           include_dirs=inc_dirs,
+                        )
 
 setup (name = 'header_wrapper',
        version = '0.1',
        author      = "thecaffiend",
        description = """Swig test for header_wrapper""",
        ext_modules = [header_wrapper_module],
-       py_modules = ["header_wrapper"],
+       py_modules = ["swig_wrapper"],
        )
